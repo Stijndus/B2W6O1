72e0d998ba7d2650a5fef609125db8ff66dfd585
Stijn <stijn@ardusit.nl>
2021-01-11 11:22:09 +0100

    Commit #2
    Fixed a bug which caused some double letters to be colored yellow. Even though the shouldnt be yellow

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
 var input = document.getElementById('input');
 var random = Math.floor(Math.random() * words.length);
 var wordSplit = words[random].split('');
+var attempt = 0;
+var wordCopy = words[random].split('');
+
+for (i = 0; i < wordSplit.length; i++) {
+    letter = document.createElement('h2');
+    letter.id = i;
+    letter.classList.add('letter')
+    document.getElementById('letters').appendChild(letter);
+    letter.innerText = i + 1;
+}
+
 document.getElementById('0').innerText = wordSplit[0];
 
-function check(){
+function check() {
     var inputWord = input.value;
     var inputArray = inputWord.split('');
-    for(i=0;i<wordSplit.length;i++){
-        letter = document.getElementById(i);
+    attempt = attempt + 1;
+    wordCopy = words[random].split('')
+    for (i = 0; i < wordSplit.length; i++) {
+        let letter = document.getElementById(i);
         letter.innerText = inputArray[i];
-        	if(wordSplit.includes(inputArray[i])){
-                if(inputArray[i]==wordSplit[i]){
-                    letter.style.background = 'red';
-                    console.log(i);
-                } else {
-                    letter.style.backgroundColor = 'yellow';
-                    console.log(i)
-                }
-            } else {
-                letter.style.background = 'grey';
-            }
+        if (wordCopy[i] === inputArray[i]) {
+            letter.style.backgroundColor = 'red';
+            wordCopy[i] = ''
+        } else if (wordCopy.includes(inputArray[i])) {
+            wordCopy[wordSplit.indexOf(inputArray[i])] = ''
+            letter.style.backgroundColor = 'yellow'
+        }
     }
 }
